3a4d8108906bf5946fe99e45582b5eae0175d3bd
Tim Kolberger <tim@kolberger.eu>
2021-02-26 22:54:56 +0100

    feat: first draft of multipart styles with classes

--- packages/system/src/use-style-config.ts
+++ packages/system/src/use-style-config.ts
@@ -1,58 +1,93 @@
+import { SystemStyleObject } from "@chakra-ui/styled-system"
 import {
-  CSSWithMultiValues,
-  RecursiveCSSObject,
-  StyleConfigOptions,
-  SystemStyleObject,
-} from "@chakra-ui/styled-system"
-import { Dict, get } from "@chakra-ui/utils"
+  filterUndefined,
+  memoizedGet as get,
+  mergeWith,
+  runIfFn,
+  omit,
+} from "@chakra-ui/utils"
+import { useMemo, useRef } from "react"
+import isEqual from "react-fast-compare"
 import { useChakra } from "./hooks"
 import { ThemingProps } from "./system.types"
-import {
-  interpretStyleConfig,
-  normalizeStyleConfig,
-} from "@chakra-ui/styled-system"
 
 export function useStyleConfig(
   themeKey: string,
-  props: ThemingProps & Dict,
-  /**
-   * @deprecated option `isMultiPart` is removed in favor of `isArray(styleConfig.parts)`
-   */
+  props: ThemingProps,
   opts: { isMultiPart: true },
 ): Record<string, SystemStyleObject>
 
 export function useStyleConfig(
   themeKey: string,
-  props?: ThemingProps & Dict,
-  /**
-   * @deprecated option `isMultiPart` is removed in favor of `isArray(styleConfig.parts)`
-   */
+  props?: ThemingProps,
   opts?: { isMultiPart?: boolean },
 ): SystemStyleObject
 
-export function useStyleConfig(
-  themeKey: string,
-  props: ThemingProps & Dict = {},
-  _: any,
-) {
+export function useStyleConfig(themeKey: any, props: any = {}, opts: any = {}) {
+  const { styleConfig: styleConfigProp, ...rest } = props
+
   const { theme, colorMode } = useChakra()
-  const { styleConfig, ...rest } = props
-  const config: StyleConfigOptions =
-    styleConfig ?? get(theme, `components.${themeKey}`)
-  const getStyles = interpretStyleConfig(
-    colorMode,
-    theme,
-    normalizeStyleConfig(config),
+  const themeStyleConfig = get(theme, `components.${themeKey}`)
+  const styleConfig = styleConfigProp || themeStyleConfig
+
+  const mergedProps = mergeWith(
+    { theme, colorMode },
+    styleConfig?.defaultProps ?? {},
+    filterUndefined(omit(rest, ["children"])),
   )
-  return getStyles(rest)
+
+  /**
+   * Store the computed styles in a `ref` to avoid unneeded re-computation
+   */
+  type StylesRef = SystemStyleObject | Record<string, SystemStyleObject>
+  const stylesRef = useRef<StylesRef>({})
+
+  return useMemo(() => {
+    if (styleConfig) {
+      const baseStyles = runIfFn(styleConfig.baseStyle ?? {}, mergedProps)
+
+      const variants = runIfFn(
+        styleConfig.variants?.[mergedProps.variant] ?? {},
+        mergedProps,
+      )
+
+      const sizes = runIfFn(
+        styleConfig.sizes?.[mergedProps.size] ?? {},
+        mergedProps,
+      )
+
+      const styles = mergeWith({}, baseStyles, sizes, variants)
+
+      if (opts?.isMultiPart && styleConfig.parts) {
+        styles.root = {}
+
+        styleConfig.parts.forEach((part: string) => {
+          styles[part] = styles[part] ?? {}
+
+          // draft for class based approach
+          const partClassName = createMultiPartClassName(themeKey, part)
+          styles.root[`.${partClassName}`] = styles[part] ?? {}
+          delete styles[part]
+        })
+      }
+
+      const isStyleEqual = isEqual(stylesRef.current, styles)
+
+      if (!isStyleEqual) {
+        stylesRef.current = styles
+      }
+    }
+
+    return stylesRef.current
+  }, [styleConfig, mergedProps, opts?.isMultiPart])
 }
 
-export function useMultiStyleConfig(
-  themeKey: string,
-  props: ThemingProps & Dict,
-) {
-  return useStyleConfig(themeKey, props) as Record<
-    string,
-    Record<string, SystemStyleObject>
-  >
+export function useMultiStyleConfig(themeKey: string, props: any) {
+  return useStyleConfig(themeKey, props, { isMultiPart: true })
+}
+
+export function createMultiPartClassName(themeKey: string, part: string) {
+  return `chakra-${themeKey}__${part}`
+    .replace(/\s/g, "") // in case there is some whitespace
+    .toLowerCase()
 }
